ebb08b5a15c578966f7556ad34ead6a74a3ee78d
Mehdi Nassim KHODJA <18899702+naskio@users.noreply.github.com>
2022-02-17 12:51:29 +0100

    add async multi download using asyncio

--- naskpy/download.py
+++ naskpy/download.py
@@ -5,17 +5,19 @@ from pathlib import Path
 import grequests
 import urllib.request
 import urllib.error
+import asyncio
+import aiohttp
 
 
-def g_download_multiple(urls: list[str], filepaths: list[Path]) -> Iterator[Tuple[str, Path, bool]]:
-    """download multiple files in parallel.
+def g_download_multiple(urls: list[str], filepaths: list[Path]) -> list[Tuple[str, Path, bool]]:
+    """Download multiple files in parallel.
 
     :param urls: list of urls to download
     :type urls: list[str]
     :param filepaths: list of filenames to save to
     :type filepaths: list[Path]
-    :return: iterator of tuples of (url, filepath, success)
-    :rtype: Iterator[Tuple[str, Path, bool]]
+    :return: list of tuples of (url, filepath, success)
+    :rtype: list[Tuple[str, Path, bool]]
     """
 
     def exception_handler(request, exception):
@@ -25,17 +27,17 @@ def g_download_multiple(urls: list[str], filepaths: list[Path]) -> Iterator[Tupl
     responses = grequests.map(responses, exception_handler=exception_handler)
     results = []
     for i, (response, filepath) in enumerate(zip(responses, filepaths)):
-        with open(filepath, 'wb') as f:
-            if response:
+        if response:
+            with open(filepath, 'wb') as f:
                 f.write(response.content)
                 results.append(True)
-            else:
-                results.append(False)
-    return zip(urls, filepaths, results)
+        else:
+            results.append(False)
+    return list(zip(urls, filepaths, results))
 
 
 def download(url: str, filepath: Path) -> bool:
-    """download the file from the url and save it to file.
+    """Download the file from the url and save it to file.
 
     :param url: url to download
     :type url: str
@@ -48,3 +50,77 @@ def download(url: str, filepath: Path) -> bool:
         return True
     except urllib.error.URLError:
         return False
+
+
+async def _async_download(session: aiohttp.ClientSession, url: str, filepath: Path) -> Tuple[str, Path, str, bool]:
+    """Async download a file from url and save it to filepath.
+
+    :param session: aiohttp session
+    :type session: aiohttp.ClientSession
+    :param url: url to download
+    :type url: str
+    :param filepath: file to save to
+    :type filepath: Path
+    :return: tuple of (url, filepath, message, success)
+    """
+    # try:
+    async with session.get(url) as response:
+        # if response.status == 200:
+        if response.ok:  # response.status < 400
+            content = await response.read()
+            with open(filepath, 'wb') as f:
+                f.write(content)
+            return url, filepath, str(response.status), True
+        else:
+            return url, filepath, str(response.status), False
+    # except Exception as e:
+    #     return url, filepath, e, False
+
+
+async def _async_download_multiples(urls: list[str], filepaths: list[Path]) -> Iterator[Tuple[str, Path, str, bool]]:
+    """Download multiple files concurrently.
+
+    :param urls: list of urls to download
+    :type urls: list[str]
+    :param filepaths: list of filenames to save to
+    :type filepaths: list[Path]
+    :return: iterator of tuples of (url, filepath, message, success)
+    :rtype: Iterator[Tuple[str, Path, str, bool]]
+    """
+    # async with aiohttp.ClientSession(raise_for_status=True) as session:
+    async with aiohttp.ClientSession() as session:
+        downloads = [_async_download(session, urls[i], filepaths[i]) for i in range(len(urls))]
+        results = []
+        for dl in asyncio.as_completed(downloads):
+            result = await dl
+            print(result)
+            results.append(result)
+        return results
+
+
+def _download_multiple_async_wrapper(urls: list[str], filepaths: list[Path]) -> Iterator[Tuple[str, Path, str, bool]]:
+    """Async wrapper for download multiple files concurrently."""
+    loop = asyncio.get_event_loop()
+    results = loop.run_until_complete(_async_download_multiples(urls, filepaths))
+    return results
+
+
+def download_multiple(urls: list[str], filepaths: list[Path], chunk_size: int = None) -> \
+        Iterator[Tuple[str, Path, str, bool]]:
+    """Download multiple files concurrently, chunk by chunk.
+
+    :param urls: list of urls to download
+    :type urls: list[str]
+    :param filepaths: list of filenames to save to
+    :type filepaths: list[Path]
+    :param chunk_size: number of files to download at once
+    :type chunk_size: int, optional
+    :rtype: Iterator[Tuple[str, Path, str, bool]]
+    """
+    if not chunk_size:
+        return _download_multiple_async_wrapper(urls, filepaths)
+    results = []
+    for i in range(0, len(urls), chunk_size):
+        print(f'Downloading {i} to {i + chunk_size}...')
+        results.extend(_download_multiple_async_wrapper(urls[i:i + chunk_size], filepaths[i:i + chunk_size]))
+    return results
--- tests/test_download.py
+++ tests/test_download.py
@@ -2,7 +2,7 @@ import unittest
 from pathlib import Path
 import shutil
 
-from naskpy.download import g_download_multiple, download
+from naskpy.download import g_download_multiple, download, download_multiple
 
 BASE_DIR = Path(__file__).parent.absolute()
 TEST_DIR = BASE_DIR / "testing_data" / 'download'
@@ -19,20 +19,55 @@ class DownloadTestCase(unittest.TestCase):
         urls = [
             "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png",
             "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png",
+            "https://www.google.com/images/branding/googlelogo/2x/sadasdas.png",
         ]
         filepaths = [
-            self.dir / 'g_1.png',
-            self.dir / 'g_2.png',
+            self.dir / 'gd_1.png',
+            self.dir / 'gd_2.png',
+            self.dir / 'gd_3.png',
+        ]
+        expected = [
+            True,
+            True,
+            False,
         ]
         g_download_multiple(urls, filepaths)
-        for filepath in filepaths:
-            self.assertTrue(filepath.exists())
+        for i in range(len(urls)):
+            self.assertEqual(expected[i], filepaths[i].exists())
 
     def test_download(self):
-        url = "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
-        path = self.dir / "d.png"
-        download(url, path)
-        self.assertTrue(path.exists())
+        urls = [
+            "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png",
+            "https://www.google.com/images/branding/googlelogo/2x/sadasdas.png",
+        ]
+        filepaths = [
+            self.dir / 'd_1.png',
+            self.dir / 'd_2.png',
+        ]
+        expected = [
+            True,
+            False,
+        ]
+        for i in range(len(urls)):
+            download(urls[i], filepaths[i])
+            self.assertEqual(expected[i], filepaths[i].exists())
+
+    def test_download_multiple(self):
+        urls = [
+            "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png",
+            "https://www.google.com/images/branding/googlelogo/2x/sadasdas.png",
+        ]
+        filepaths = [
+            self.dir / 'dm_1.png',
+            self.dir / 'dm_2.png',
+        ]
+        expected = [
+            True,
+            False,
+        ]
+        download_multiple(urls, filepaths)
+        for i in range(len(urls)):
+            self.assertEqual(expected[i], filepaths[i].exists())
 
     @classmethod
     def tearDownClass(cls):
