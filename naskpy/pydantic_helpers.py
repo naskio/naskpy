723bbc8d41f1ce07aca6561c48389868266ceeea
Mehdi Nassim KHODJA <18899702+naskio@users.noreply.github.com>
2022-02-17 09:05:29 +0100

    add: pydantic helpers

---
+++ naskpy/pydantic_helpers.py
@@ -0,0 +1,20 @@
+from unittest import TestCase
+
+
+def show_diff(instance1, instance2) -> None:
+    """show difference between two pydantic instances
+
+    :param instance1: pydantic instance
+    :type instance1: pydantic.BaseModel
+    :param instance2: pydantic instance
+    :type instance2: pydantic.BaseModel
+    :rtype: None
+    """
+    if instance1 == instance2:
+        print("EQUAL.")
+    else:
+        print("DIFFERENT:")
+        try:
+            TestCase().assertDictEqual(instance1.dict(), instance2.dict())
+        except Exception as e:
+            print(e)
